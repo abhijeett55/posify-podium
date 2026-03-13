d7f6664822212fbfeb28e667665f9d6a61747d88
abhijeett55 <100180572+abhijeett55@users.noreply.github.com>
2026-03-14 01:20:50 +0530

    fixes bugs and problem with UI

--- src/app/_components/layout/layout.ts
+++ src/app/_components/layout/layout.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Header } from '../header/header';
+import { Footer } from '../footer/footer';
 import { Sidebar } from '../sidebar/sidebar';
 
 @Component({
   selector: 'app-layout',
-  imports: [ CommonModule, RouterModule, Sidebar ],
+  imports: [ CommonModule, RouterModule, Sidebar, Header, Footer ],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
